79e673ac20ef130f1aa6d5205f931c6cd80b3aac
Solange Mendes <appsleanittest@gmail.com>
2021-08-25 10:19:20 +0200

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,52 @@
-  
-    <!-- Tooltip script-->
-    <!--<script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-    </script>-->
-    
-    <!-- JS sxcript for play and pause buttons-->
-    <!--<script>
-        $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 2000 } );
-            $("#carousel-pause").click(function(){
-                $("#mycarousel").carousel('pause');
-            });
-            $("#carousel-play").click(function(){
-                $("#mycarousel").carousel('cycle');
-            });
-        });
-    </script>-->
+/* Tooltip script*/
+/*$(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });*/
 
-    <!-- JS script for play and pause with one button-->
-    <script>-->
-            $("#carouselButton").click(function(){
-            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-                $("#mycarousel").carousel('pause');
-                $("#carouselButton").children("span").removeClass('fa-pause');
-                $("#carouselButton").children("span").addClass('fa-play');
-            }
-            else if ($("#carouselButton").children("span").hasClass('fa-play')){
-                $("#mycarousel").carousel('cycle');
-                $("#carouselButton").children("span").removeClass('fa-play');
-                $("#carouselButton").children("span").addClass('fa-pause');                    
-            }
+/* JS script for play and pause buttons*/
+/*$(document).ready(function(){
+        $("#mycarousel").carousel( { interval: 2000 } );
+        $("#carousel-pause").click(function(){
+            $("#mycarousel").carousel('pause');
         });
-    </script>-->
+        $("#carousel-play").click(function(){
+            $("#mycarousel").carousel('cycle');
+        });
+    });*/
+
+/* JS script for play and pause with one button*/
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        } else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    }); 
 
 
-    <!-- JS script for open the login modal-->
-    <script>
-        $(document).ready(function(){
-            $("#loginid").click(function(){
-                $("#loginModal").modal('toggle');
-            });
+/* JS script for open the login modal*/
+    $(document).ready(function() {
+        $("#loginid").click(function() {
+            $("#loginModal").modal('toggle');
         });
-    </script>
+    }); 
 
-    <!-- JS script for open the table reservation modal-->
-    <script>
-        $(document).ready(function(){
-            $("#Reservebtn").click(function(){
-                $("#ReserveModal").modal('toggle');
-            });
+
+/* JS script for open the table reservation modal*/
+    $(document).ready(function() {
+        $("#Reservebtn").click(function() {
+            $("#ReserveModal").modal('toggle');
         });
-    </script>
-    
-    <!--<script>
-        $("#loginid").click(function(){
-                $('#loginModal').modal('show')
-            });
-        $("#Reservebtn").click(function(){
-                $('#ReserveModal').modal('show')
-            });
-    </script>-->
+    }); 
+
+
+/*$("#loginid").click(function(){
+            $('#loginModal').modal('show')
+        });
+    $("#Reservebtn").click(function(){
+            $('#ReserveModal').modal('show')
+        });*/
